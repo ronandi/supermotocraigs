2de01dc1dd57f152f5c4fa4e613ee4a2b48fc52e
Rohit Kumar <rohit.kumar@rutgers.edu>
2013-02-10 03:24:03 -0500

    ready to deply

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var craigslist = require('./lib/craigslist')
+var growl = require('growl');
 var Mongolian = require('mongolian');
 var CENTRAL_NJ = 'http://cnj.craigslist.org/search/mca?hasPic=1&minAsk=3000&query=supermoto&srchType=A&format=rss';
 var JERSEY_SHORE = 'http://jerseyshore.craigslist.org/search/mca?hasPic=1&minAsk=3000&query=supermoto&srchType=A&format=rss';
@@ -6,12 +7,9 @@ var NORTH_JERSEY = 'http://newjersey.craigslist.org/search/mca?hasPic=1&minAsk=3
 var SOUTH_JERSEY = 'http://southjersey.craigslist.org/search/mca?hasPic=1&minAsk=3000&query=supermoto&srchType=A&format=rss';
 var NJRSS = [CENTRAL_NJ, JERSEY_SHORE, NORTH_JERSEY, SOUTH_JERSEY];
 
+var DB_HOST = process.env.MONGOLAB_URI || 'localhost'
 var server = new Mongolian();
-var db = server.db('test-supermoto-db');
-
-function printResults(results) {
-    console.log(results);
-}
+var db = server.db('supermoto-db');
 
 function storeNewResults(results) {
     var latestId = -1;
@@ -21,13 +19,10 @@ function storeNewResults(results) {
             if (count > 0) {
                 latestId  = array[0].id;
             }
-            var count = 0;
             for (var i = 0; i < results.length; i++) {
                 var result = results[i];
                 db.collection('results').insert(result);
-                count++;
             }
-            console.log(count + " new results found");
         });
     });
 }
@@ -43,10 +38,20 @@ function checkFeeds(callback) {
 
 function runScript() {
     db.collection('results').ensureIndex({ id: 1 }, { unique: true })
-
     console.log("Checking feeds...");
     checkFeeds();
-    setInterval(checkFeeds, 30000);
+    setInterval(checkFeeds, 180000);
+    db.collection('results').count(function(count) {
+        function notifyIfNew() {
+            db.collection('results').count(function(newcount) {
+                if (newcount > count) {
+                    count = newcount;
+                    growl('New listings found!', { title: 'Craigslist' });
+                }
+            });
+        }
+        setInterval(notifyIfNew, 300000);
+    });
 }
 
 runScript();
