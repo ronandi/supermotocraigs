b3400780d0591ff11c3d5746165a8af1fb4af02b
Rohit Kumar <rohit.kumar@rutgers.edu>
2013-02-10 05:13:46 -0500

    clean up logging

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var craigslist = require('./lib/craigslist')
-var growl = require('growl');
 var Mongolian = require('mongolian');
 var CENTRAL_NJ = 'http://cnj.craigslist.org/search/mca?hasPic=1&minAsk=3000&query=supermoto&srchType=A&format=rss';
 var JERSEY_SHORE = 'http://jerseyshore.craigslist.org/search/mca?hasPic=1&minAsk=3000&query=supermoto&srchType=A&format=rss';
@@ -8,14 +7,24 @@ var SOUTH_JERSEY = 'http://southjersey.craigslist.org/search/mca?hasPic=1&minAsk
 var NJRSS = [CENTRAL_NJ, JERSEY_SHORE, NORTH_JERSEY, SOUTH_JERSEY];
 
 var DB_HOST = process.env.MONGOLAB_URI || 'localhost'
-var server = new Mongolian();
+var server = new Mongolian({
+    log: {
+        debug: function(message) {},
+        info: function(message) {},
+        warn: function(message) {
+            console.log(message);
+        },
+        error: function(message) {
+            console.log(message);
+        }
+    }
+});
 var db = server.db('supermoto-db');
 
 function storeNewResults(results) {
     var latestId = -1;
     db.collection('results').find().sort({ date: -1 }).toArray(function(err, array) {
         db.collection('results').count(function(err, count) {
-            console.log(count + " results already stored.");
             if (count > 0) {
                 latestId  = array[0].id;
             }
@@ -28,17 +37,15 @@ function storeNewResults(results) {
 }
 
 function checkFeeds(callback) {
-    console.log("============================")
     console.log("[" + new Date + "] Starting RSS scrape...");
     NJRSS.forEach(function(feed) {
+        console.log("Scraping " + feed);
         craigslist.getListingsFromRss(feed, storeNewResults);
     });
-    console.log("============================")
 }
 
 function runScript() {
     db.collection('results').ensureIndex({ id: 1 }, { unique: true })
-    console.log("Checking feeds...");
     checkFeeds();
     setInterval(checkFeeds, 180000);
     db.collection('results').count(function(count) {
@@ -46,7 +53,7 @@ function runScript() {
             db.collection('results').count(function(newcount) {
                 if (newcount > count) {
                     count = newcount;
-                    growl('New listings found!', { title: 'Craigslist' });
+                    console.log('New listings found!');
                 }
             });
         }
